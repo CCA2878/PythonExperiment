09dfdd7195623859facb0522d4ba40695addbae1
CCA2878 <1054331608@qq.com>
2021-03-04 14:22:42 +0800

    2021-3-4 14:22:40

--- 2021-3-4_1325.py
+++ 2021-3-4_1325.py
@@ -1,10 +1,10 @@
 #2021-3-4 13:28:17 tempconvert.py
 tempStr = input('Temp with C or F: ')
-if tempStr[-1] in ['F','f']:
+if tempStr[-1] in ['F','f11']:
     result = (eval(tempStr[0:-1]) - 32) / 1.8
     print('Result is {:.2f}C'.format(result))
 elif tempStr[-1] in ['C','c']:
     result = 1.8 * eval(tempStr[0:-1]) + 32
     print('Result is {:.2f}F'.format(result))
 else:
-    print('Input Error!')
+    print('嘤嘤嘤!')
