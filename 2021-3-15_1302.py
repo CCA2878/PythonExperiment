7695e8b4deec3faa664938c0262cc2416b9173bb
CCA2878 <1054331608@qq.com>
2021-03-16 14:56:27 +0800

    2021-3-16 14:56:25

--- 2021-3-15_1302.py
+++ 2021-3-15_1302.py
@@ -3,13 +3,13 @@ from random import *
 turtle1 = turtle
 turtle1.setup(400,300,50,50)
 turtle1.colormode(255)
-turtle1.speed(1)
+turtle1.speed(2)
 i = 40
 while i >= 1:
     turtle1.pensize(i)
     turtle1.pencolor((randint(0,255),randint(0,255),randint(0,255)))
     turtle1.fd(100)
     turtle1.lt(90)
-    i += -4
+    i += -2
 
 input()
