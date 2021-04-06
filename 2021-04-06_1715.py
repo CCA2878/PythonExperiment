55e3c4e2c233049551bf4287d1d9f211444fd2c9
CCA2878 <1054331608@qq.com>
2021-04-07 01:32:45 +0800

    2021-04-07 01:32:44

--- 2021-04-06_1715.py
+++ 2021-04-06_1715.py
@@ -1,15 +1,12 @@
 num = (input()).split(',')
-if float(num[0]) == 0 and float(num[1]) == 0:
-    print(f'点（{num[0]}，{num[1]}）是坐标原点。')
-elif float(num[0]) == 0 and float(num[1]) != 0:
-    print(f'点（{num[0]}，{num[1]}）在y轴上。')
-elif float(num[0]) != 0 and float(num[1]) == 0:
-    print(f'点（{num[0]}，{num[1]}）在x轴上。')
-elif float(num[0]) > 0 and float(num[1]) > 0:
-    print(f'点（{num[0]}，{num[1]}）属于第一象限。')
-elif float(num[0]) < 0 and float(num[1]) > 0:
-    print(f'点（{num[0]}，{num[1]}）属于第二象限。')
-elif float(num[0]) < 0 and float(num[1]) < 0:
-    print(f'点（{num[0]}，{num[1]}）属于第三象限。')
-elif float(num[0]) > 0 and float(num[1]) < 0:
-    print(f'点（{num[0]}，{num[1]}）属于第四象限。')
+if float(num[0]) + float(num[1]) <= float(num[2]) or float(num[0]) + float(num[2]) <= float(num[1]) or float(num[1]) + float(num[2]) <= float(num[0]):
+    print(f'边长{num[0]},{num[1]},{num[2]}不能构成三角形。')
+elif float(num[0]) == float(num[1]) and float(num[0]) == float(num[2]):
+    S = ((4 * float(num[1]) ** 2 * float(num[2]) ** 2 - (float(num[0]) ** 2 - float(num[1]) ** 2 -float(num[2]) ** 2) ** 2) ** 0.5) * 0.25
+    print('边长{},{},{}能构成等边三角形，它的面积是{:.1f}。'.format(num[0], num[1], num[2], S))
+elif float(num[0]) ** 2 + float(num[1]) ** 2 == float(num[2]) ** 2 or float(num[0]) ** 2 + float(num[2]) ** 2 == float(num[1]) ** 2 or float(num[1]) ** 2 + float(num[2]) ** 2 == float(num[0]) ** 2:
+    S = ((4 * float(num[1]) ** 2 * float(num[2]) ** 2 - (float(num[0]) ** 2 - float(num[1]) ** 2 -float(num[2]) ** 2) ** 2) ** 0.5) * 0.25
+    print('边长{},{},{}能构成直角三角形，它的面积是{:.1f}。'.format(num[0], num[1], num[2], S))
+else:
+    S = ((4 * float(num[1]) ** 2 * float(num[2]) ** 2 - (float(num[0]) ** 2 - float(num[1]) ** 2 -float(num[2]) ** 2) ** 2) ** 0.5) * 0.25
+    print('边长{},{},{}不能构成直角三角形，也不能构成等边三角形，它的面积是{:.1f}。'.format(num[0], num[1], num[2], S))
