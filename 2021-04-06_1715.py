e1853dc4f2eabf57eb6a9c6ebbe060f5a392a7cd
CCA2878 <1054331608@qq.com>
2021-04-07 00:57:47 +0800

    2021-04-07 00:57:45

--- 2021-04-06_1715.py
+++ 2021-04-06_1715.py
@@ -1,4 +1,7 @@
-sth = input()
-if '0' <= sth <= '9':print('numeric')
-elif 'A' <= sth <= 'Z' or 'a' <= sth <= 'z':print('alpha')
-else:print('other')
+num = (input()).split(',')
+if float(num[0]) <= 0 or float(num[1]) <= 0 or num[0].count('.') != 0 or num[1].count('.') != 0:
+    print('输入数据错误！')
+elif float(num[0]) % float(num[1]) == 0.0:
+    print(f'{num[1]}是{num[0]}的因数。')
+else:
+    print(f'{num[1]}不是{num[0]}的因数。')
