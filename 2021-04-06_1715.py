9a542d0ee373080433f393fdae7f1bb22db4c8ee
CCA2878 <1054331608@qq.com>
2021-04-07 01:06:11 +0800

    2021-04-07 01:06:10

--- 2021-04-06_1715.py
+++ 2021-04-06_1715.py
@@ -1,7 +1,15 @@
 num = (input()).split(',')
-if float(num[0]) <= 0 or float(num[1]) <= 0 or num[0].count('.') != 0 or num[1].count('.') != 0:
-    print('输入数据错误！')
-elif float(num[0]) % float(num[1]) == 0.0:
-    print(f'{num[1]}是{num[0]}的因数。')
-else:
-    print(f'{num[1]}不是{num[0]}的因数。')
+if float(num[0]) == 0 and float(num[1]) == 0:
+    print(f'点（{num[0]}，{num[1]}）是坐标原点。')
+elif float(num[0]) == 0 and float(num[1]) != 0:
+    print(f'点（{num[0]}，{num[1]}）在y轴上。')
+elif float(num[0]) != 0 and float(num[1]) == 0:
+    print(f'点（{num[0]}，{num[1]}）在x轴上。')
+elif float(num[0]) > 0 and float(num[1]) > 0:
+    print(f'点（{num[0]}，{num[1]}）属于第一象限。')
+elif float(num[0]) < 0 and float(num[1]) > 0:
+    print(f'点（{num[0]}，{num[1]}）属于第二象限。')
+elif float(num[0]) < 0 and float(num[1]) < 0:
+    print(f'点（{num[0]}，{num[1]}）属于第三象限。')
+elif float(num[0]) > 0 and float(num[1]) < 0:
+    print(f'点（{num[0]}，{num[1]}）属于第四象限。')
