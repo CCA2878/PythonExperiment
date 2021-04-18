1910f43ad31a62a926fa7fa9d317eba00d04f2e7
CCA2878 <1054331608@qq.com>
2021-04-18 17:14:23 +0800

    2021-04-18 17:14:22

--- 2021-04-18_1625.py
+++ 2021-04-18_1625.py
@@ -1,6 +1,13 @@
 inputStr = input()
 newStr = []
+s = 0
 for i in range(len(inputStr)):
-    if not ord('A') <= ord(inputStr[i]) <= ord('Z'):
+    if ord('A') <= ord(inputStr[i]) <= ord('Z'):
+        newStr.append(chr(ord(inputStr[i]) + 32))
+        s += 1
+    elif ord('a') <= ord(inputStr[i]) <= ord('z'):
+        newStr.append(chr(ord(inputStr[i]) - 32))
+        s += 1
+    else:
         newStr.append(inputStr[i])
-print(''.join(newStr))
+print(f'共输入{s}个字母,替换后为"' + ''.join(newStr) + '".')
