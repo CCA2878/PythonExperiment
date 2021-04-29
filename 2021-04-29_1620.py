29fd9a79eaa2e5c9ea376129699b1a19333329d5
CCA2878 <1054331608@qq.com>
2021-04-29 16:59:21 +0800

    2021-04-29 16:59:20

--- 2021-04-29_1620.py
+++ 2021-04-29_1620.py
@@ -1,7 +1,5 @@
-s = 0
-for i in range(1, 94):#1分钱
-    for j in range(1, 48):#2分钱
-        for k in range(1, 20):#5分钱
-            if i + (2 * j) + (5 * k) == 100:
-                s += 1
-print(s)
+for i in range(1, 10):
+    for j in range(i, 10):
+        print('{}*{}={:2}'.format(j, i, i * j), end = '  ')
+    if i != 9:
+        print('\n' + ('        ' * i), end="")
