05f174572a53e0f0a5034cfe326abc660e37ff54
CCA2878 <1054331608@qq.com>
2021-04-29 17:10:11 +0800

    2021-04-29 17:10:11

--- 2021-04-29_1620.py
+++ 2021-04-29_1620.py
@@ -1,5 +1,3 @@
-for i in range(1, 5):
+for i in range(1, 6):
     print(' ' * (5 - i) + '*' * (2 * i - 1))
-print('*********')
-for i in range(4, 0, -1):
-    print(' ' * (5 - i) + '*' * (2 * i - 1))
+print('   ***\n' * 8, end = '')
