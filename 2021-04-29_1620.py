2cba84b0d8f2571ce2a6c8ba8fc0199092e38bd3
CCA2878 <1054331608@qq.com>
2021-04-29 19:47:09 +0800

    2021-04-29 19:47:06

--- 2021-04-29_1620.py
+++ 2021-04-29_1620.py
@@ -1,3 +1,23 @@
-for i in range(1, 6):
-    print(' ' * (5 - i) + '*' * (2 * i - 1))
-print('   ***\n' * 8, end = '')
+level = int(input())
+n = [[1], [1, 1]]
+for i in range(2, level):#生成数列列表
+    n.append([1])
+    for j in range(i - 1):
+        n[i].append(n[i - 1][j] + n[i - 1][j + 1])
+    n[i].append(1)
+
+for str1 in n:#按傻逼到不能再傻逼的格式输出
+    print(' ' * ((level - n.index(str1) - 1) * 3 - 1), end = '')
+    if n.index(str1) != level - 1:
+        if n.index(str1) == 0:
+            for str2 in str1:
+                print('{:3}'.format(str2), end = '')
+            print(' ')
+        else:
+            for str2 in str1:
+                print('{:3}'.format(str2), end = '   ')
+            print(' ')
+    else:
+        for str2 in str1:
+            print('{:2}'.format(str2), end = '    ')
+        print('')
