8e174afe1ca4779673b652180e698b90fa90a36e
CCA2878 <1054331608@qq.com>
2021-05-10 13:41:44 +0800

    2021-05-10 13:41:43

--- 2021-05-10_1309.py
+++ 2021-05-10_1309.py
@@ -1,8 +1,11 @@
-def Prime(num):
-    if num == 1:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
-print(Prime(int(input())))
+def triAngle(inputStr):
+    try:
+        num = (inputStr).split(',')
+        if float(num[0]) + float(num[1]) <= float(num[2]) or float(num[0]) + float(num[2]) <= float(num[1]) or float(num[1]) + float(num[2]) <= float(num[0]) or float(num[0]) <= 0 or float(num[1]) <= 0 or float(num[2]) <= 0:
+            return 0.00
+        else:
+            return ((4 * float(num[1]) ** 2 * float(num[2]) ** 2 - (float(num[0]) ** 2 - float(num[1]) ** 2 -float(num[2]) ** 2) ** 2) ** 0.5) * 0.25
+    except:
+        return 0.00
+
+print('{:.2f}'.format(triAngle(input())))
