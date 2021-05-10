98d19f1e6d3810e55685bac921a91469580d789f
CCA2878 <1054331608@qq.com>
2021-05-10 13:48:58 +0800

    2021-05-10 13:48:57

--- 2021-05-10_1309.py
+++ 2021-05-10_1309.py
@@ -1,11 +1,8 @@
-def triAngle(inputStr):
-    try:
-        num = (inputStr).split(',')
-        if float(num[0]) + float(num[1]) <= float(num[2]) or float(num[0]) + float(num[2]) <= float(num[1]) or float(num[1]) + float(num[2]) <= float(num[0]) or float(num[0]) <= 0 or float(num[1]) <= 0 or float(num[2]) <= 0:
-            return 0.00
-        else:
-            return ((4 * float(num[1]) ** 2 * float(num[2]) ** 2 - (float(num[0]) ** 2 - float(num[1]) ** 2 -float(num[2]) ** 2) ** 2) ** 0.5) * 0.25
-    except:
-        return 0.00
+def fact(n):
+    s = 1
+    for i in range(1, n + 1):
+        s = s * i
+    return s
 
-print('{:.2f}'.format(triAngle(input())))
+num = input().split(',')
+print('{:.2f}'.format(fact(int(num[0])) / (fact(int(num[1])) * fact(int(num[0]) - int(num[1])))))
